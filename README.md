febc5f5b5b7191c3ce68918026897eaad5a11407
AdriellyVitoria <mendes.silva.vitoria.adrielly@gmail.com>
2023-01-31 14:40:12 -0300

    imersao alura

---
+++ README.md
@@ -0,0 +1,17 @@
+# Primeira aula da Imersão Dev da plataforma Alura
+
+# Conteúdo detalhado desta aula
+Criar uma conta no CodePen;
+Entender as diferenças entre HTML, CSS e JavaScript;
+Variáveis, manipulação dos valores armazenados e a memória do computador;
+Tipos de variáveis, como texto e inteiro;
+Fixando a quantidade de casas decimais com a função toFixed().
+
+
+# Desafios desta aula!
+Adicionar outras moedas para converter;
+Conversor de quilômetros para anos luz e verificar o tempo que demora para ir de uma estrela para outra;
+Conversor de temperaturas entre fahrenheit, kelvin e celsius;
+Adicionar uma linha ao projeto desenvolvido para que apareça o valor em bitcoin.
+
+*Finalizando os primeiros desafios com sucesso*
--- dia1.js
+++ dia1.js
@@ -1,32 +1,36 @@
-let opção = 0
-while (opção <= 5) {
-    opção = window.prompt("Gostaria da conversão para: \n1-> Converso de moedas\n2-> Quilômetros para anos luz \n3-> Conversor de temperaturas\n4-> Valor em bitcoin\n5-> Sair ");
+let opcao = 0
+function verificar() {
+    
+}
+while (opcao <= 5) {
+    opcao = window.prompt("Gostaria da conversão para: \n1-> Converso de moedas\n2-> Quilômetros para anos luz \n3-> Conversor de temperaturas\n4-> Valor em bitcoin\n5-> Sair ");
 
-    if (opção == 1) {
+    if (opcao == 1) {
         let valorParaTransformar = window.prompt('Qual o valor desejando para a conversao');
-        
+
         let cotacaoDoDolar = 5.32;
         let valorEmDolar = valorParaTransformar * cotacaoDoDolar;
         valorEmDolar = valorEmDolar.toFixed(2)
 
-        let euroCotação = 5.55;
-        let conversãoMoeda = valorParaTransformar * euroCotação;
+        let euroCotacao = 5.55;
+        let conversãoMoeda = valorParaTransformar * euroCotacao;
         conversãoMoeda = conversãoMoeda.toLocaleString('pt-BR');
     
         alert('O valor ' + valorParaTransformar + ' em dolar é ' + valorEmDolar + '\nE em euro é € ' + conversãoMoeda);
     }
 
-    else if (opção == 2) {
-        let valorAnosLuz = prompt("Qual valor em Anos-Luz você quer converter para quilômetros?")
+    else if (opcao == 2) {
+        let valorQuilometroEmNumero = prompt("Qual o valor em  km que vc quer transformar em anos luz?")
 
-        parseFloat(valorAnosLuz);
+       let anosLuz = 9.460536207068015;
+       resultando = valorQuilometroEmNumero * anosLuz;
+       resultando = resultando.toFixed(4)
+
         
-        let valorEmKm = valorAnosLuz * 9.460536207068015
-        
-        alert(valorEmKm); //colocar uma descrição
+        alert( 'Este valor ' + valorQuilometroEmNumero + ' E equivalente a ' + resultando + 'anos luz\nA cada 1 ano luz e equivalente a 10 TRILHÕES de km'); 
     }
 
-    else if (opção == 3) {
+    else if (opcao == 3) {
         let temperatura = prompt("Qual a temperatura em Celson, vc desejar mudar ?");
         let tempFahrenheit = (temperatura * 9) / 5 + 32;
         let tempKelvin = temperatura + 273.15;
@@ -34,14 +38,18 @@ while (opção <= 5) {
         alert('A temperatura em fahrenheit é ' + tempFahrenheit + '\nA temperatura em Kelvin é ' + tempKelvin);
     }
 
-    else if (opção == 4) {
-        let numeroParaTransformar = prompt("Qual o numero que vc deseja converte: ");
-        let cotaçãoBTC = 116500.71;
-        let valorBTC = numeroParaTransformar * cotaçãoBTC;
+    else if (opcao == 4) {
+        let numeroParaTransformar = prompt("Qual o numero que vc deseja converte para bitcoin: ");
+        let cotacaoBTC = 116500.71;
+        let valorBTC = numeroParaTransformar * cotacaoBTC;
 
         valorBTC = valorBTC.toLocaleString('pt-BR');
 
-        alert(valorBTC)
+        alert('O valor ' + numeroParaTransformar + ' em bitcoin é ' + valorBTC)
+    }
+    else if (opcao == 5) {
+        alert('Obrigada por usar nossos serviços. Volte sempre')
+        break
     }
     else {
         alert('O número informando é inválido, tente novamente')
@@ -49,4 +57,4 @@ while (opção <= 5) {
 
 }
 
-alert('Obrigada por usar nosso serviços. Volte sempre')
+
